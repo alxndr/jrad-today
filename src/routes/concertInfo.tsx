a3e09ab25573f2ab2e19ca43bc673ea6afe5cf1c
Alexander <alxndr+github@gmail.com>
2022-09-22 13:31:07 -0700

    feat: date selector
    [fixes #3]
    
    also...
    * make TypeScript happy (?)
    * Qwik wants to know when a prop is expected to change, use `mutable`
    * some style tweaks
    * forgot to commit the `papaparse` dep

--- src/routes/concertInfo.tsx
+++ src/routes/concertInfo.tsx
@@ -2,7 +2,15 @@ import { component$, useStore, useStylesScoped$ } from '@builder.io/qwik'
 
 import styles from './concertInfo.css?inline'
 
-export default component$(({data: {id, recordings, tagline}}) => {
+export function recordingTypeAbbrev(type: string) {
+  switch (type) {
+    case 'soundboard': return 'SBD'
+    case 'audience': return 'AUD'
+    default: return type
+  }
+}
+
+export default component$(({data: {id, recordings, tagline}}: any) => {
   const store = useStore({
     showSetlist: false,
   })
@@ -13,10 +21,10 @@ export default component$(({data: {id, recordings, tagline}}) => {
     <a href={showUrl} target="_blank">
       {tagline}
     </a>
-    {recordings?.length && <ul class="component-concertInfo--recordings">{recordings.map?.(recording =>
-      <li><a href={recording.url}>{recording.type}</a></li>
+    {recordings?.length && <ul class="component-concertInfo--recordings">{recordings.map?.((recording: {type: string, url: string}) =>
+      <li><a href={recording.url}>{recordingTypeAbbrev(recording.type)}</a></li>
     )}</ul>}
-    <button onClick$={() => store.showSetlist = !Boolean(store.showSetlist)}>{store.showSetlist ? 'hide' : 'show'} setlist</button>
+    <button onClick$={() => store.showSetlist = !store.showSetlist}>{store.showSetlist ? 'hide' : 'show'} setlist</button>
     {store.showSetlist
       ? <iframe src={setlistUrl}></iframe>
       : false
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
-import { component$, useClientEffect$, useStylesScoped$, useStore, useWatch$ } from '@builder.io/qwik'
+import { component$, mutable, useClientEffect$, useStylesScoped$, useStore, useWatch$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import ppp from 'papaparse'
 
 import styles from './home.css?inline'
-import ConcertInfo from './concertInfo.tsx'
+import ConcertInfo from './concertInfo'
 
-export function monthNumToWord(m) {
-  switch (m) {
+export function monthNumToWord(monthNum: number) {
+  switch (monthNum) {
     case 1: return 'January';
     case 2: return 'February';
     case 3: return 'March';
@@ -19,57 +19,63 @@ export function monthNumToWord(m) {
     case 10: return 'October';
     case 11: return 'November';
     case 12: return 'December';
-    default: throw new Error('Unrecognized month number: ${m}')
+    default: throw new Error(`Unrecognized month number: ${monthNum}`)
   }
 }
 
 export default component$(() => {
   useStylesScoped$(styles);
-  const store = useStore({
+  const nowDateObj = new Date()
+  const store = useStore<any>({
+    concertData: null,
+    day: nowDateObj.getDate(),
     isJsRunning: false,
+    month: nowDateObj.getMonth() + 1,
+    recordingsData: null,
+    todaysConcerts: null,
   })
   useClientEffect$(async () => {
     store.isJsRunning = true
-    const nowDateObj = new Date()
-    const dayNum = 11||nowDateObj.getDate()
-    const monthNum = 11||nowDateObj.getMonth() + 1
-    store.md = `${monthNum}/${dayNum}`
-    store.month_day = `${monthNumToWord(monthNum)} ${dayNum}`
-  })
-  useWatch$(async ({ track }) => {
-    track(store, 'md')
-    if (store.md) {
-      ppp.parse('https://github.com/alxndr/almost-dead-net/raw/main/src/data/csv/shows.csv', {header: true, download: true, worker: false, complete: (results) => {
+    ppp.parse('https://github.com/alxndr/almost-dead-net/raw/main/src/data/csv/shows.csv',
+      {header: true, download: true, worker: false, complete: (results: {data: any}) => {
         store.concertData = results?.data
-        ppp.parse('https://github.com/alxndr/almost-dead-net/raw/main/src/data/csv/recordings.csv', {header: true, download: true, worker: false, complete: (results) => {
-          store.recordingsData = results?.data
-        }})
-      }})
-    }
+        ppp.parse('https://github.com/alxndr/almost-dead-net/raw/main/src/data/csv/recordings.csv',
+          {header: true, download: true, worker: false, complete: (results: {data: any}) => {
+            store.recordingsData = results?.data
+          }}
+        )
+    }})
   })
   useWatch$(async ({track}) => {
-    track(store, 'recordingsData')
-    if (!store.md) return
-    const todayWithASlash = `${store.md}/`
+    const recordingsData = track(store, 'recordingsData')
+    if (!recordingsData) return;
+    const day = track(store, 'day')
+    const month = track(store, 'month')
+    const dateWithoutYear = `${month}/${day}/`
     store.todaysConcerts = store.concertData
-      ?.filter?.(concert => concert?.date?.startsWith(todayWithASlash) && concert.set1) // filtering on set1 means we don't see shows which have not yet occurred... https://github.com/alxndr/jrad-today/issues/1
-      ?.sort((a, b) => a.id - b.id)
-      ?.map(concert => ({
+      ?.filter?.((concert: {date: string, set1: string}) => concert?.date?.startsWith(dateWithoutYear) && concert.set1) // filtering on set1 means we don't see shows which have not yet occurred... https://github.com/alxndr/jrad-today/issues/1
+      ?.sort((a: {id: string}, b: {id: string}) => Number(a.id) - Number(b.id))
+      ?.map((concert: {id: string}) => ({
         ...concert,
-        recordings: store.recordingsData?.filter?.(recording => recording.show === concert.id)
+        recordings: recordingsData?.filter?.((recording: {show: string}) => recording.show === concert.id)
       }))
   })
 
+  const tWord = store.day === new Date().getDate() && store.month - 1 === new Date().getMonth()
+    ? 'Today'
+    : 'Then'
+
   return (
     <div class="page-home">
-      <h1>Today in <a href="http://www.joerussosalmostdead.com" target="_blank">JRAD</a> History
-        {store.month_day ? `: ${store.month_day}` : ''}
+      <h1>
+        {tWord} {' '}
+        in <a href="http://www.joerussosalmostdead.com" target="_blank">JRAD</a> History: {monthNumToWord(store.month)} {store.day}
       </h1>
       {store.isJsRunning
         ? store.todaysConcerts
           ? store.todaysConcerts?.length
-            ? store.todaysConcerts.map?.(concert =>
-                <ConcertInfo data={concert} />
+            ? store.todaysConcerts.map?.((concert: any) =>
+                <ConcertInfo data={mutable(concert)} />
               )
             : <>
                 <p>Aw, nothing played on this date yet...</p>
@@ -79,6 +85,16 @@ export default component$(() => {
             <p>Get info about a past JRAD concert performed on this day, once you enable JavaScript!</p>
           </noscript>
       }
+      {store.isJsRunning && <p>
+        Try another date?
+        <input name="month" placeholder="month" type="number" min="1" max="12" value={store.month} onClick$={(_event, element: {value: string}) => {
+          store.month = Number(element.value)
+        }} />
+        /
+        <input name="day" placeholder="day" type="number" min="1" max="31" value={store.day} onClick$={(_event, element: {value: string}) => {
+          store.day = Number(element.value)
+        }} />
+      </p>}
       <p>Chat about all things JRAD at <a href="https://lot.almost-dead.net/" target="_blank">The Lot</a>!</p>
     </div>
   );
